2e7d021c45c5742678ee3275b45898d0a682d649
sh3zTheAlien <ioannismrgl23@gmail.com>
2024-10-26 23:33:35 +0300

    Add files via upload

--- task.py
+++ task.py
@@ -1,9 +1,73 @@
-print("Welcome to the tip calculator!")
-bill = float(input("What was the total bill? $"))
-tip = int(input("What percentage tip would you like to give? 10 12 15 "))
-people = int(input("How many people to split the bill? "))
-payment = (bill / people) * (1 + (tip / 100))
-print(f"Each person should pay: ${payment:.2f}")
+import random
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
 
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+#Mine solution
+computer_choose = random.randint(0, 2)
+user_input = input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n")
+if user_input == "0":
+    print(rock,"Computer chose:")
+
+    if computer_choose == 0:
+        print(rock)
+        print("It's a draw.")
+    elif computer_choose == 1:
+        print(paper)
+        print("You lose")
+    else:
+        print(scissors)
+        print("You win!")
+
+elif user_input == "1":
+    print(paper, "Computer chose:")
+
+    if computer_choose == 0:
+        print(rock)
+        print("You win!")
+    elif computer_choose == 1:
+        print(paper)
+        print("It's a draw")
+    else:
+        print(scissors)
+        print("You lose")
+
+elif user_input == "2":
+    print(scissors, "Computer chose:")
+
+    if computer_choose == 0:
+        print(rock)
+        print("You lost")
+    elif computer_choose == 1:
+        print(paper)
+        print("You Win!")
+    else:
+        print(scissors)
+        print("It's a draw")
+
+else:
+    print("You entered wrong input rerun the program.")
 
 
